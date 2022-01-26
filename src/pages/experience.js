51a4616361df9aa74639406993f106e65c1c65e9
Kori Reine <kkr.reine@gmail.com>
2022-01-26 16:41:45 -0600

    update experience

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -10,16 +10,22 @@ const ExperiencePage = () => (
     <Seo title="experience" />
     <h1>Experience</h1>
     <ExperienceItem
+      title="Website Developer"
+      company="Three Divas Gift Shop"
+      dates="January 2022 - Present"
+      descr="Developing a website to display the gift shop's products to reach a wider audience and bring in new customers"
+    />
+    <ExperienceItem
+      title="C# Tutor"
+      company="Self"
+      dates="January 2022"
+      descr="Teaching C# programming fundamentals through lectures and homework assistance to a game design student in a month-long introduction to C# course"
+    />
+    <ExperienceItem
       title="Video Engineer Trainee"
       company="Atheist Community of Austin"
       dates="November 2021 - Present"
       descr="Currently training to be a (volunteer) video engineer for the 501(c)3 organization's multiple live streams using Wirecast"
-    />
-    <ExperienceItem
-      title="Recruiting Assistant"
-      company="New York Life & NYLife Securities LLC"
-      dates="May 2019 - August 2019"
-      descr="As assistant to a partner, increased the number of interviews and hires to exceed quota"
     />
     <ExperienceItem
       title="Online Store and Social Media Assistant"
@@ -28,7 +34,13 @@ const ExperiencePage = () => (
       descr="Built a website through Shopify that increased sales for the business and expanded sales to nationwide"
     />
     <ExperienceItem
-      title="Childcare Provider"
+      title="Recruiting Assistant"
+      company="New York Life & NYLife Securities LLC"
+      dates="May 2019 - August 2019"
+      descr="As assistant to a partner, increased the number of interviews and hires to exceed quota"
+    />
+    <ExperienceItem
+      title="Tutor / HomeSchool Teacher / Childcare Provider"
       company="Self / St. Catherine of Siena School"
       dates="October 2018 - April 2021"
       descr="Assisted families and an elementary school for 15-25 hours per week while enrolled full-time in school"
